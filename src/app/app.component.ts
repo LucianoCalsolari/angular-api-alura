9b61bd65d85f3f48cfac9c0997ee53d62150ad2c
LucianoCalsolari <souzaluciano9517@yahoo.com.br>
2020-12-24 20:03:44 -0300

    Integração com API

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { PhotoService } from './photos/photo/photo.service';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+
+})
+
+export class AppComponent implements OnInit{
+  
+  photos: any[] = [];
+  
+  constructor(private photoService : PhotoService){}
+  
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+    
+    this.photoService
+    .listFromUser('flavio')
+    .subscribe(photos => this.photos = photos);
+    
+  }
+  
+
+}
