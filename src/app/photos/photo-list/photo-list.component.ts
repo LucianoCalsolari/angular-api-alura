0536a968520074831658426f5df6f076b552624d
LucianoCalsolari <souzaluciano9517@yahoo.com.br>
2020-12-27 20:00:16 -0300

    Reformulação dos arquivos em submodulos

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -37,7 +37,7 @@ export class PhotoListComponent implements OnInit,OnDestroy {
   load(){
     this.photoService.listFromUserPaginated(this.userName, ++this.currentPage)
     .subscribe(photos=>{
-      this.photos= this.photos.concat(photos));
+      this.photos= this.photos.concat(photos);
       if(!photos.length) this.hasMore = false;
     });
   }
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -8,19 +8,14 @@ import { PhotoFormComponent } from './photo-form/photo-form.component';
 import { PhotosComponent } from './photo-list/photos/photos.component';
 import { FilterByDescription } from './photo-list/filter-by-description.pipe';
 import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
+import { PhotoFormModule } from './photo-form/photo-form.module';
+import { PhotoListModule } from './photo-list/photo-list.module';
 
 @NgModule({
-    declarations: [
-        PhotoComponent, 
-        PhotoListComponent,
-        PhotoFormComponent,
-        PhotosComponent,
-        FilterByDescription,
-        LoadButtonComponent
-    ],
     imports:[
-        HttpClientModule,
-        CommonModule
+        PhotosModule,
+        PhotoFormModule,
+        PhotoListModule
     ]
 })
 
