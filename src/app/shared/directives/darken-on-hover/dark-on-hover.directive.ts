2ca25786d1adf6408e4eb8ffab82d4e3b81e7e54
LucianoCalsolari <souzaluciano9517@yahoo.com.br>
2020-12-28 22:41:17 -0300

    Adicionando a diretiva de hover darken

--- src/app/shared/directives/darken-on-hover/dark-on-hover.directive.ts
+++ src/app/shared/directives/darken-on-hover/dark-on-hover.directive.ts
@@ -1,14 +1,18 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input, Renderer2 } from '@angular/core';
 
 @Directive({
     selector: '[apDarkenOnHover]'
 })
 export class DarkenOnHoverDirective{
-    constructor(private el: ElementRef){}
+
+    @Input() brightness = '70%'
+
+    constructor(private el: ElementRef,
+        private render: Renderer2){}
 
     @HostListener('mouseover')
     darkenOn(){
-
+        this.render.setStyle(this.el.nativeElement,'filter','brightness(${this.brightness})');
     }
 
     @HostListener('mouseleave')
